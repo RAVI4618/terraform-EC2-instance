24a1797c97fdc7c876b44d3d10a2d6fe0e40e126
RAVI4618 <124133106+RAVI4618@users.noreply.github.com>
2025-04-04 15:57:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_subnet" "my_public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "ap-south-1" # Change if needed
+  availability_zone = "ap-south-1a" # Change if needed
 
   tags = {
     Name = "My-Public-Subnet"
